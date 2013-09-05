c7f9414b80f1fb40e7be8fd3fea017e7105e2c9c
Mikel Larreategi <mlarreategi@codesyntax.com>
2013-09-05 10:52:48 +0200

    configure a new viewlet manager

--- bise/theme/browser/interfaces.py
+++ bise/theme/browser/interfaces.py
@@ -1,5 +1,7 @@
-from zope.interface import Interface
 from plone.theme.interfaces import IDefaultPloneLayer
+from zope.interface import Interface
+from zope.viewlet.interfaces import IViewletManager
+
 
 class IThemeSpecific(IDefaultPloneLayer):
     """Marker interface that defines a Zope 3 browser layer.
@@ -7,8 +9,15 @@ class IThemeSpecific(IDefaultPloneLayer):
        "BISETheme" theme, this interface must be its layer
        (in theme/viewlets/configure.zcml).
     """
+
+
 class IBISEView(Interface):
     """ """
 
     def getColumnsClass():
         """ Returns the CSS class based on columns presence. """
+
+
+class IHtmlHeadTitle(IViewletManager):
+    """A viewlet manager for the title of the page
+    """
