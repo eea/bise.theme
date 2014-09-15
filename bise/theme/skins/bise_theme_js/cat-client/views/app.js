ff89c0ab7fb6523d7ff8c50389ffb10099a01809
MikelS <mikel.santamaria@gmail.com>
2014-09-15 13:12:21 +0200

    Missin file

--- bise/theme/skins/bise_theme_js/cat-client/views/app.js
+++ bise/theme/skins/bise_theme_js/cat-client/views/app.js
@@ -46,11 +46,13 @@ define(['jquery', 'underscore', 'backbone', 'jqcloud', 'bootstrap', 'collections
                 'mergeFacet', 'removeFacet', 'isFacetSelected',
                 'enableAdvancedSearch', 'enableBiseSearch')
 
+      // Fix for IE8
       $.support.cors = true;
       this._checkIE();
 
       // Add main template
       $(this.$el.selector).append(this.mainTemplate)
+      this.queryparams.indexes = Object.keys(this.bise_indexes);
 
       this.host = options['host']
       this.refreshEndpoint()
@@ -60,9 +62,8 @@ define(['jquery', 'underscore', 'backbone', 'jqcloud', 'bootstrap', 'collections
       if (q != 'undefined' && q != '') this.queryparams.query = q
 
       // Run QUERY by default
-      this.queryparams.indexes = Object.keys(this.bise_indexes);
       this.runQuery()
-      // this._renderStatistics()
+      this._renderStatistics()
     },
 
     // Minor fix to allow Object.keys in IE8
@@ -95,7 +96,7 @@ define(['jquery', 'underscore', 'backbone', 'jqcloud', 'bootstrap', 'collections
     },
 
     fillQueryAndRun: function(e){
-      //console.log(':: fillQueryAndRun')
+      console.log(':: fillQueryAndRun')
       e.preventDefault()
       var q = $('#catalogue-search-form #query').val()
       if (q=='') q = this.$('#catalogue-queries li').html()
@@ -109,6 +110,7 @@ define(['jquery', 'underscore', 'backbone', 'jqcloud', 'bootstrap', 'collections
     },
 
     runQuery: function(){
+      console.log(':: runQuery..')
       this.Results.fetch({ data: $.param(this.queryparams) })
     },
 
@@ -122,20 +124,17 @@ define(['jquery', 'underscore', 'backbone', 'jqcloud', 'bootstrap', 'collections
       this.queryparams.page = 1
       this.queryparams.per = parseInt($(e.target).html());
       this.runQuery()
-      // this.Results.fetch({ data: $.param(this.queryparams) })
     },
     goPrevPage: function(e){
       if (this.queryparams.page > 1){
         this.queryparams.page -= 1;
         this.runQuery()
-        // this.Results.fetch({ data: $.param(this.queryparams) })
       }
     },
     goNextPage: function(e){
       if (this.queryparams.page < this._getLastPage()){
         this.queryparams.page += 1;
         this.runQuery()
-        // this.Results.fetch({ data: $.param(this.queryparams) })
       }
     },
     enableBiseSearch: function(e){
@@ -281,35 +280,72 @@ define(['jquery', 'underscore', 'backbone', 'jqcloud', 'bootstrap', 'collections
       }
     },
 
+    _drawSuggestions: function(){
+      // TODO: Implement suggestions from ElasticSearch
+      // if (this.Results.suggestions){
+      //   var opts = this.Results.suggestions[1];
+      //   if (opts.length > 0){
+      //     opts = opts[0]
+      //     $('.catalogue-suggestions ul').html('')
+      //     for (var i=0; i < opts.options.length; i++){
+      //       var suggestion = opts.options[i];
+      //       var li = $('<li>').html(suggestion.text + ' with '+suggestion.score+'score')
+      //       $('.catalogue-suggestions').append(li)
+      //     }
+      //   }
+      // }
+    },
+
+    _showResults: function(){
+      this.$('.catalogue-container').show()
+      this.$('.catalogue-no-results').hide()
+      this.$('.catalogue-statistics').hide()
+      this.$('.catalogue-available-content').hide()
+      this._drawPagination()
+    },
+
     _showNoResults: function(){
+      this.$('.catalogue-container').hide()
       this.$('.catalogue-no-results').show()
-      this.$('.catalogue-content').hide()
+      this.$('.catalogue-statistics').show()
+      this.$('.catalogue-available-content').show()
       // if (this.queryparams.query && this.queryparams.query.length > 0)
       //   this.$('.catalogue-no-results').html('No results found.')
       // else
       //   this.$('.catalogue-no-results h1').html('')
     },
 
-    _showResults: function(){
-      this.$('.catalogue-no-results').hide()
-      this.$('.catalogue-content').show()
-      this._drawPagination()
-    },
 
     // ------------------------  Cloud ----------------------------------------
 
-    // _renderStatistics: function(){
-    //   $.get("http://bise.catalogue.dev/api/v1/stats.json", function( data ) {
-    //     $('.catalogue-cloud-tags').jQCloud(data.tags);
-    //     console.log(data)
+    _renderStatistics: function(){
+      console.log("http://"+this.host+"/api/v1/stats.json")
+      $.get("http://"+this.host+"/api/v1/stats.json", function( data ) {
+        // Show cloud tags
+        $('.catalogue-cloud-tags').jQCloud(data.tags);
+        // Render last added content
+        _.each(data.last, function(item){
 
-    //     for (var i=0; i < data.last.length ; i++) {
-    //       console.log(data.last[i])
-    //       $('.catalogue-last-added').append( $('<li>').html(data.last[i].title))
-    //     };
+          var cell = $('<li>').addClass('catalogue-cell')
+          var link = $('<a>').attr('href', item.link).html(item.title)
+          cell.append($('<div>').addClass('cell-title').append(link))
 
-    //   });
-    // },
+          var subtitle = $('<div>').addClass('cell-subtitle')
+          subtitle.append($('<strong>').html(item.type))
+          subtitle.append('&nbsp;').append(item.published_on)
+          cell.append(subtitle)
+
+          // $('.catalogue-last-added').append($('<li>').html(link))
+          $('.catalogue-last-added').append(cell)
+
+          // this.$("#catalogue-results").append(view.render().el)
+        });
+        // Render statistics
+        _.each(Object.keys(data.counts), function(item, count){
+          $('.catalogue-available-content .span2.'+item+' > h1').html(count)
+        });
+      });
+    },
 
     // ------------------------  FACETS ----------------------------------------
 
@@ -337,10 +373,12 @@ define(['jquery', 'underscore', 'backbone', 'jqcloud', 'bootstrap', 'collections
     // ------------------------  RENDER ----------------------------------------
 
     render: function() {
+      console.log(':: render...')
       this._drawSearches();
       this._drawCount();
       this._drawCategories();
       this._drawFacets();
+      this._drawSuggestions();
       if (this.Results.total == 0) this._showNoResults(); else this._showResults();
     },
     addOne: function(result) {
