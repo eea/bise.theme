891979809adcd2b55f524a1d67bf1ef864b3038d
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2017-02-15 11:11:08 +0200

    Refs #80293 Bise; fix color for plone.app.iterate warning messages

--- bise/theme/skins/bise_theme_js/bise.theme.js
+++ bise/theme/skins/bise_theme_js/bise.theme.js
@@ -378,4 +378,9 @@ $(document).ready(function(){
       });
         }
       }
+
+  // fix warning messages from plone.app.iterate
+  if($(".portalMessage.info:contains('Warning')").length){
+    $('.portalMessage.info').removeClass('info').addClass('warning');
+  }
 });
